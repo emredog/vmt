2615372449d15f6e3357f5a13dfc17ecfcc972b9
emredog <emredog@gmail.com>
2014-02-07 18:46:10 +0200

    Replaced input from video to image sequence. Removed Video class and ffmpg dependencies. Compiles OK. DOESNT TEST IT YET!!!

---
+++ Klaser-Schmid_Hog3D_qt/opencv/imagesequence.cpp
@@ -0,0 +1,135 @@
+#include "imagesequence.h"
+
+// std
+#include <stdexcept>
+
+// my stuff
+#include <opencv/IplImageWrapper.h>
+//#include <opencv/functions.h>
+//#include <numeric/functions.hpp>
+
+const bool DEBUGOUT = false;
+
+
+void ImageSequence::init()
+{
+    // prepare...
+    QStringList filters;
+    if (DEPTH == 1)
+        filters << ".jp2";
+    else
+        filters << ".jpg";
+    // ...and get all "jp2" (depth) files, ordered by name
+    this->imageFiles = this->directory.entryList(filters, QDir::Files | QDir::NoDotAndDotDot, QDir::Name);
+
+    // throw an error if something went wrong
+    if (this->imageFiles.isEmpty())
+        throw std::runtime_error("Error initializing image sequence!");
+
+    // get width and height of images
+    // TODO
+    if (DEBUGOUT) std::cout << "### Width: " << this->_width <<  " Height: " << this->_height << std::endl;
+
+    // get the first frame, then we know the starting time of the first frame
+    //if (DEBUGOUT) std::cout << "### startPts: " << _startPts << std::endl;
+    this->advance(0);
+    //_startPts = _currentFrameIndex;
+//    if (DEBUGOUT) std::cout << "### startPts: " << _startPts << std::endl;
+}
+
+ImageSequence::~ImageSequence()
+{
+    this->cleanup();
+}
+
+const IplImageWrapper ImageSequence::getFrame() const
+{
+    if (this->_currentBuffer)
+        return *(this->_currentBuffer);
+    else
+        return IplImageWrapper();
+}
+
+bool ImageSequence::nextFrame()
+{
+    return this->advance(this->_currentFrameIndex + 1);
+}
+
+bool ImageSequence::prevFrame()
+{
+    return this->advance(this->_currentFrameIndex - 1);
+}
+
+bool ImageSequence::seek(ImageSequence::FrameIndex frame)
+{
+    return this->advance(frame);
+}
+
+bool ImageSequence::probeFrame(ImageSequence::FrameIndex frame)
+{
+    return this->advance(frame);
+}
+
+
+
+void ImageSequence::cleanup()
+{
+    // free all allocated objects
+//    if (this->_peekBuffer)
+//        delete this->_peekBuffer;
+    if (this->_currentBuffer)
+        delete this->_currentBuffer;
+
+    // reset internal variables
+//    _startPts = 0;
+    _currentFrameIndex = 0;
+//    _peekFrameIndex = 0;
+    _width = 0;
+    _height = 0;
+
+    this->imageFiles.clear();
+}
+
+
+bool ImageSequence::advance(FrameIndex frameIndex)
+{
+    int frame = static_cast<int>(frameIndex);
+    // I don't check "whether we need to retrieve the next frame", I simply retrieve it
+    QString currentFile;
+    if (frame < this->imageFiles.size())
+    {
+        currentFile = this->imageFiles[frame];
+        this->_currentFrameIndex = static_cast<FrameIndex>(frame);
+    }
+    else
+    {
+        return false;
+    }
+//    frame++;
+
+//    if (frame < this->imageFiles.size())
+//    {
+//        peekFile = this->imageFiles[frame];
+//        this->_peekFrameIndex = frame;
+//    }
+
+
+    // if allocated, delete it.
+    if (this->_currentBuffer)
+    {
+        delete this->_currentBuffer;
+        this->_currentBuffer = NULL;
+    }
+
+    // create a new image
+    if (DEPTH == 1)
+        this->_currentBuffer = new IplImageWrapper(currentFile.toStdString(), CV_16UC1); // 16 bit depth images (jp2)
+    else
+        this->_currentBuffer = new IplImageWrapper(currentFile.toStdString(), CV_8UC3); // 8 bit RGB images (jpg)
+
+    this->_width = this->_currentBuffer->getWidth();
+    this->_height = this->_currentBuffer->getHeight();
+
+    return true;
+}
+
---
+++ Klaser-Schmid_Hog3D_qt/opencv/imagesequence.h
@@ -0,0 +1,73 @@
+#ifndef IMAGESEQUENCE_H
+#define IMAGESEQUENCE_H
+
+#include <QDir>
+
+#define DEPTH 1
+
+// forward decleration
+class IplImageWrapper;
+
+class ImageSequence
+{
+public:
+    typedef /*long*/ long FrameIndex;
+protected:
+    QDir directory;
+    QStringList imageFiles;
+
+    IplImageWrapper *_currentBuffer;
+    //IplImageWrapper *_peekBuffer;
+
+    int _width;
+    int _height;
+    //long long _startPts;
+    FrameIndex _currentFrameIndex;
+    //FrameIndex _peekFrameIndex;
+
+    void init();
+    void cleanup();
+    bool advance(FrameIndex frameIndex);
+
+public:
+    //constructor
+    ImageSequence(const std::string& folderPath) :
+        directory(QString::fromStdString(folderPath)),
+       /* _startPts(0),*/ _currentFrameIndex(0), _currentBuffer(NULL) /*, _peekFrameIndex(0)*/
+    {
+        init();
+    }
+
+    //desctructor
+    ~ImageSequence();
+
+    // queries
+    const std::string& getFileName() const
+    {
+        return this->directory.absolutePath().toStdString();
+    }
+
+    int getWidth() const { return this->_width; }
+    int getHeight() const { return this->_height; }
+    //double getDuration() const;
+    FrameIndex numOfFrames() const { return static_cast<FrameIndex>(this->imageFiles.count()); }
+    FrameIndex getFrameIndex() const { return this->_currentFrameIndex; }
+    //double getFrameRate() const;
+    //double getTime() const;
+    //double getStartTime() const;
+
+    // frame access
+    const IplImageWrapper getFrame() const;
+
+    // seeking functionality
+    bool nextFrame();
+    bool prevFrame();
+    bool seek(FrameIndex frame);
+    //bool seek(double seconds);
+
+    // other functionalities
+    bool probeFrame(FrameIndex frame);
+    //bool probeFrame(double seconds);
+};
+
+#endif // IMAGESEQUENCE_H
