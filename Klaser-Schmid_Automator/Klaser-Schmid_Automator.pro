b58dc880ddbdd731a154323075355c19da1fd054
emredog <emredog@gmail.com>
2014-02-25 14:13:08 +0200

    It's an application who runs Klaser-Schmid_Hog3D_qt program for all videos and track files in the dataset.

---
+++ Klaser-Schmid_Automator/Klaser-Schmid_Automator.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-02-25T10:21:49
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = Klaser-Schmid_Automator
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
---
+++ Klaser-Schmid_Automator/main.cpp
@@ -0,0 +1,129 @@
+#include <QCoreApplication>
+#include <QProcess>
+#include <QDir>
+#include <QHash>
+#include <iostream>
+//#include <stdio.h>
+
+using std::cout;
+using std::cerr;
+using std::endl;
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    //-----------------------------------------------------------------------------------------------------------------
+    QDir dataDir("/home/emredog/LIRIS-data/training-validation/");
+    QDir targetDir("/home/emredog/LIRIS-data/training-validation_features/");
+    QDir::setCurrent("/home/emredog/qt_builds/build-Klaser-Schmid_Hog3D_qt-Desktop-Release/");
+    QString program = "./Klaser-Schmid_Hog3D_qt";
+
+    QStringList algoArgs;
+    algoArgs << "-P" <<  "icosahedron"
+             << "--loose-track"
+             << "--xy-stride" <<  "16"
+             << "--t-stride" << "16"
+             << "--t-ncells" << "2"
+             << "--xy-scale" << "1"
+             << "--t-scale" << "1"
+             << "--scale-overlap" << "1"
+             << "--npix" << "2";
+    //-----------------------------------------------------------------------------------------------------------------
+
+    cout << "Data directory: " << dataDir.absolutePath().toStdString() << endl;
+    QStringList nameFilters;
+
+    //get all image sequences:
+    QStringList videos = dataDir.entryList(nameFilters, QDir::AllDirs | QDir::NoDotAndDotDot);
+    cout << "Videos fetched: " << videos.count() << endl;
+
+    //get all track files
+    nameFilters << "*.track";
+    QStringList trackFiles = dataDir.entryList(nameFilters, QDir::Files | QDir::NoDotAndDotDot);
+    cout << "Track files fetched: " << trackFiles.count() << endl;
+
+    //match track files and videos:
+    cout << "Matching track files and videos";
+
+    QHash<QString, QString> videosToTrackFiles;
+    foreach (QString video, videos)
+    {
+        QStringList searchResults = trackFiles.filter(video);
+        foreach (QString result, searchResults)
+            videosToTrackFiles.insertMulti(video, result);
+
+        cout << ".";
+    }
+
+    cout << endl << "Matching completed." << endl;
+
+    QHash<QString, QString>::const_iterator it = videosToTrackFiles.constBegin();
+    QStringList inputArgs;
+    QFileInfo* outputFileInfo;
+    int counter = 1;
+    int total = videosToTrackFiles.count();
+
+    QProcess* process;
+
+    while (it != videosToTrackFiles.constEnd())
+    {
+        //set input arguments with video name and track file name
+        inputArgs << "--video-file" << dataDir.absoluteFilePath(it.key())
+                 << "-t" << dataDir.absoluteFilePath(it.value());
+
+        //create a new process
+        process = new QProcess();
+        //set output file name
+        outputFileInfo = new QFileInfo(it.value());
+        process->setStandardOutputFile(targetDir.absoluteFilePath(outputFileInfo->baseName() + ".out"), QIODevice::Truncate);
+
+        //run the program
+        process->start(program, inputArgs + algoArgs);
+
+        //check if it started normally
+        if (!process->waitForStarted(-1))
+        {
+            cerr << "Could not start process with following parameters:" << endl
+                 << "input: " << inputArgs.join(" ").toStdString() << endl
+                 << "parameters: " << algoArgs.join(" ").toStdString() << endl
+                 << "output: " << targetDir.absoluteFilePath(outputFileInfo->baseName() + ".out").toStdString() << endl;
+
+            continue;
+        }
+
+        cout << "Process " << counter << " started..." << endl;
+        cout << "\tExpected output file: " << targetDir.absoluteFilePath(outputFileInfo->baseName() + ".out").toStdString() << endl << endl;
+
+        //wait 10 minutes for it to finish
+        if (!process->waitForFinished(600000)) //wait for 10 minutes
+        {
+            cerr << "Could not complete process within 10 minutes, with following parameters:" << endl
+                 << "input: " << inputArgs.join(" ").toStdString() << endl
+                 << "parameters: " << algoArgs.join(" ").toStdString() << endl
+                 << "output: " << targetDir.absoluteFilePath(outputFileInfo->baseName() + ".out").toStdString() << endl;
+
+            continue;
+        }
+
+        cout << "Process " << counter << " completed. Remaining: " << total-counter << endl << endl;
+        counter++;
+
+        //clean-up
+        delete process;
+        delete outputFileInfo;
+        inputArgs.clear();
+    }
+
+
+
+
+    //TODO: set arguments
+
+
+    //TODO: manage process starts / ends etc.
+    //QProcess *myProcess = new QProcess();
+    //myProcess->start(program, arguments);
+
+    return a.exec();
+}
