ed9521e03590717962b90907d813e57ad710b4a6
emredog <emredog@gmail.com>
2014-04-20 21:06:10 +0300

    Massive bug was resolved :]

--- Klaser-Schmid_Automator/main.cpp
+++ Klaser-Schmid_Automator/main.cpp
@@ -14,16 +14,17 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     //-----------------------------------------------------------------------------------------------------------------
-    QDir dataDir("/home/emredog/LIRIS-data/test/");
-    QDir targetDir("/home/emredog/LIRIS-data/test_features/test_features_params02");
+    QDir dataDir("/home/emredog/LIRIS-data/training-validation/");
+    QDir trackFileDir("/home/emredog/LIRIS-data/training-validation_annotations-with-NO-ACTION/");
+    QDir targetDir("/home/emredog/LIRIS-data/training-validation_features/training-validation_withNoAction_features_params01");
     QDir::setCurrent("/home/emredog/qt_builds/build-Klaser-Schmid_Hog3D_qt-Desktop-Release/");
     QString program = "./Klaser-Schmid_Hog3D_qt";
 
     QStringList algoArgs;
     algoArgs << "-P" <<  "icosahedron"
              << "--loose-track"
-             << "--xy-stride" <<  "32"
-             << "--t-stride" << "32"
+             << "--xy-stride" <<  "16"
+             << "--t-stride" << "16"
              << "--xy-ncells" << "2"
              << "--t-ncells" << "2"
              << "--xy-scale" << "1"
@@ -40,8 +41,8 @@ int main(int argc, char *argv[])
     cout << "Videos fetched: " << videos.count() << endl;
 
     //get all track files
-    nameFilters << "*_b3_d10.track";
-    QStringList trackFiles = dataDir.entryList(nameFilters, QDir::Files | QDir::NoDotAndDotDot);
+    nameFilters << "*.track";
+    QStringList trackFiles = trackFileDir.entryList(nameFilters, QDir::Files | QDir::NoDotAndDotDot);
     cout << "Track files fetched: " << trackFiles.count() << endl;
 
     //match track files and videos:
@@ -71,7 +72,7 @@ int main(int argc, char *argv[])
     {
         //set input arguments with video name and track file name
         inputArgs << "--video-file" << dataDir.absoluteFilePath(it.key())
-                 << "-t" << dataDir.absoluteFilePath(it.value());
+                 << "-t" << trackFileDir.absoluteFilePath(it.value());
 
         //create a new process
         process = new QProcess();
