ed18e05a4c1447f5973edace95132f8483662a86
emredog <emredog@gmail.com>
2014-08-05 17:27:38 +0300

    Now forms an -UNVERIFIED- cube that is separately (XY + Z) sobel filtered cube.
    Still have to calculate the gradient vectors & mean gradient.

--- 3D_Gradient_Calculation/main.cpp
+++ 3D_Gradient_Calculation/main.cpp
@@ -10,7 +10,7 @@ int main()
 {
     //#	Size after trimming: 193x311x526, for vid0008_1_typing_716-756Union_STFiltered.pcd
     SparseMat spVmt = PointCloudFunctions::loadVmtFromPCD("/home/emredog/Documents/output/vid0008_1_typing_716-756Union_STFiltered.pcd",
-                                        193, 311, 526);
+                                                          193, 311, 526);
 
     size_t nzCount = spVmt.nzcount();
 
@@ -21,14 +21,18 @@ int main()
     cv::Range ranges[3];
     //some cube that I know there are points in it
     ranges[0] = cv::Range(40, 80);
-    ranges[1] = cv::Range(120, 160);
+    ranges[1] = cv::Range(120, 156);
     ranges[2] = cv::Range(452, 484);
 
     Mat cube = IntensityGradientComputation::crop3dMat(vmt, ranges);
 
-    SparseMat spCube(cube);
+    {
+        SparseMat spCube(cube);
 
-    nzCount = spCube.nzcount();
+        nzCount = spCube.nzcount();
+
+        spCube.release();
+    }
 
     IntensityGradientComputation::computeMeanIntensityGradient(cube);
 
--- VMT_Calculate/intensitygradientcomputation.cpp
+++ VMT_Calculate/intensitygradientcomputation.cpp
@@ -1,9 +1,12 @@
 #include "intensitygradientcomputation.h"
 #include "PointCloudFunctions.h"
 
-#include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 #include <iostream>
+
+#include <QString>
 
 cv::Vec3f IntensityGradientComputation::computeMeanIntensityGradient(const cv::SparseMat &sparseCube)
 {
@@ -19,57 +22,114 @@ cv::Vec3f IntensityGradientComputation::computeMeanIntensityGradient(const cv::M
     if (cube.type() != CV_8UC1)
         return cv::Vec3f(-1.0, -1.0, -1.0);
 
-    //Size of cube
-    int sizeX = cube.size[0]; //cols
-    int sizeY = cube.size[1]; //rows
-    int sizeZ = cube.size[2];
+    //FIXME: delete these:
+//    cv::SparseMat cubeSparse(cube);
+//    PointCloudFunctions::saveVmtAsCloud(cubeSparse, "/home/emredog/Documents/output/cube.pcd");
+//    cubeSparse.release();
 
-    cv::MatConstIterator_<uchar> _it = cube.begin<uchar>();
-    for(;_it!=cube.end<uchar>(); _it++)
-    {
-        uchar val = *_it;
-        unsigned int vall = static_cast<unsigned int>(val);
-        //std::cout << _it-> << std::endl;
-    }
+    //Size of cube
+    int sizeRows = cube.size[0];
+    int sizeCols = cube.size[1];
+    int sizeDepth = cube.size[2];
 
     //gradients: cube/dx , cube/dy , cube/dz
-    cv::Mat gradXY(cube.dims, cube.size, CV_16SC1);
-    cv::Mat gradXYZ(cube.dims, cube.size, CV_16SC1);
+    cv::Mat gradXY(cube.dims, cube.size, CV_8UC1);
+    gradXY = cv::Scalar(0);
+    //cv::Mat gradXYZ(cube.dims, cube.size, CV_8UC1/*CV_16SC1*/);
 
     //some common variables:
     cv::Range ranges[3];
-//    cv::Mat prevSlice;
-
 
     //calculate gradX and gradY: ----------------------------------------------------------------------------
     ranges[0] = cv::Range::all();
     ranges[1] = cv::Range::all();
-    for (int z = 0; z < sizeZ; z++)
+    for (int z = 0; z < sizeDepth; z++)
     {
 
-        cv::Mat currentSlice(sizeY, sizeX, CV_8UC1);
+        cv::Mat currentSlice(sizeRows, sizeCols, CV_8UC1);
         ranges[2] = cv::Range(z, z+1); //take a slice orthogonal to Z axis
 
         currentSlice = cube(ranges).clone();
+
         // create a temporarily 2d image and copy its size
         // to make our image a real 2d image
         cv::Mat out;
-        const int sizes[2] = {sizeY, sizeX};
+        const int sizes[2] = {sizeRows, sizeCols};
         out.create(2, sizes, CV_8UC1);
         currentSlice.copySize(out);
-        std::cout << currentSlice << std::endl;
 
-        cv::Sobel(currentSlice, out, CV_8UC1, 1, 1); //sobel operator on X and Y: dx = 1, dy = 1
-        gradXY(ranges) = out;
+        //QString mmm = QString("/home/emredog/Documents/output/slice/%1.jpg").arg(z);
+        //cv::imwrite(mmm.toStdString(), currentSlice);
+
+        //sobel operator on X and Y: dx = 1, dy = 1
+        cv::Sobel(currentSlice, out, CV_8UC1, 1, 1, 3, 1, 0, cv::BORDER_REFLECT_101);
+
+        //QString ppp = QString("/home/emredog/Documents/output/sobeled/%1.jpg").arg(z);
+        //cv::imwrite(ppp.toStdString(), out);
+
+        //write sobel'ed values into 3D matrix:
+        for (int x = 0; x<out.cols; x++)
+            for (int y = 0; y<out.rows; y++)
+            {
+                uchar val = out.at<uchar>(x, y);
+                if (val > 0)
+                    gradXY.at<uchar>(x, y, z) =  val;
+            }
 
         out.release();
-//        prevSlice = currentSlice;
+        currentSlice.release();
+    }
+//    cv::SparseMat gradXYSparse(gradXY);
+//    PointCloudFunctions::saveVmtAsCloud(gradXYSparse, "/home/emredog/Documents/output/cube_gradXY_sobel.pcd");
+
+
+    //calculate gradZ ---------------------------------------------------------------------------------------
+    cv::Mat gradXYZ(cube.dims, cube.size, CV_8UC1);
+    gradXYZ = cv::Scalar(0);
+
+    //think of it as if we rotated the cube around Y axis:
+    ranges[1] = cv::Range::all();
+    ranges[2] = cv::Range::all();
+    for (int x = 0; x < sizeCols; x++) //think of x as "depth"
+    {
+        cv::Mat currentSlice(sizeRows, sizeDepth, CV_8UC1); //and think of Z as "width"
+        ranges[0] = cv::Range(x, x+1); //take a slice orthogonal to X axis
+
+        currentSlice = gradXY(ranges).clone();
+
+        // create a temporarily 2d image and copy its size
+        // to make our image a real 2d image
+        cv::Mat out;
+        const int sizes[2] = {sizeRows, sizeDepth};
+        out.create(2, sizes, CV_8UC1);
+        currentSlice.copySize(out);
+
+        QString mmm = QString("/home/emredog/Documents/output/slice/XYZ_%1.jpg").arg(x);
+        cv::imwrite(mmm.toStdString(), currentSlice);
+
+        //sobel operator on X and Y: dx = 1, dy = 0 (it's actually dz = 1, since we "kinda" rotated the cube)
+        cv::Sobel(currentSlice, out, CV_8UC1, 1, 0, 3, 1, 0, cv::BORDER_REFLECT_101);
+
+        QString ppp = QString("/home/emredog/Documents/output/sobeled/XYZ_%1.jpg").arg(x);
+        cv::imwrite(ppp.toStdString(), out);
+
+        //write sobel'ed values into 3D matrix:
+        for (int z = 0; z<out.cols; z++) //note that: now out's cols actually goes up to sizeZ
+            for (int y = 0; y<out.rows; y++)
+            {
+                uchar val = out.at<uchar>(z, y);
+                if (val > 0)
+                    gradXYZ.at<uchar>(x, y, z) =  val;
+            }
+
+        out.release();
         currentSlice.release();
     }
 
-    //FIXME: delete these:
-    cv::SparseMat gradXYSparse(gradXY);
-    PointCloudFunctions::saveVmtAsCloud(gradXYSparse, "/home/emredog/Documents/output/gradXY_sobel.pcd");
+//    cv::SparseMat gradXYZSparse(gradXYZ);
+//    PointCloudFunctions::saveVmtAsCloud(gradXYZSparse, "/home/emredog/Documents/output/cube_gradXYZ_sobel.pcd");
+
+
 
 
 
