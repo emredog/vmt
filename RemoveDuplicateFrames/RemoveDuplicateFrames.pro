4d354b5dd0d61e71d3af63265f0628f3256b6271
emredog <emredog@gmail.com>
2014-10-08 11:21:03 +0300

    Basic script to remove duplicate frames captured via KinectCapture_libfreenect. It's based on exact byte size of the frames.

---
+++ RemoveDuplicateFrames/RemoveDuplicateFrames.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-10-08T11:03:58
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = RemoveDuplicateFrames
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
---
+++ RemoveDuplicateFrames/main.cpp
@@ -0,0 +1,54 @@
+#include <QCoreApplication>
+#include <QDir>
+#include <QStringList>
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    if (argc != 2)
+    {
+        cout << "Usage: ./RemoveDuplicateFiles <dir>\n";
+        return -1;
+    }
+
+    QString argument =  QString::fromAscii(argv[1]);
+
+    QDir dirToClean(argument);
+
+    QString uff = dirToClean.absolutePath();
+
+    QStringList filters;
+    filters << "*.png";
+
+    QStringList entries = dirToClean.entryList(filters, QDir::Files | QDir::NoDotAndDotDot, QDir::Name);
+
+    cout << "Processing files: " << entries.length() << endl;
+
+
+    quint64 prevSize = 0;
+
+    int removeCounter = 0;
+
+    foreach (QString fileName, entries)
+    {
+        QFile file(dirToClean.absoluteFilePath(fileName));
+        quint64 size = file.size();
+
+        if (size == prevSize)
+        {
+            removeCounter++;
+            file.remove();
+        }
+
+        prevSize = size;
+    }
+
+    cout << "Duplicates removed: " << removeCounter << endl << endl;
+
+    return 0;
+}
