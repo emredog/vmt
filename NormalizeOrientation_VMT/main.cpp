e2befbab72d041fffd219bd968d80a1d6a397f83
emredog <emredog@gmail.com>
2014-10-23 11:31:26 +0300

    Rotation and translation functions works fine.

--- NormalizeOrientation_VMT/main.cpp
+++ NormalizeOrientation_VMT/main.cpp
@@ -1,8 +1,40 @@
 #include <QCoreApplication>
 
+#include "opencv/vmt.h"
+#include "orientationnormalizer.h"
+
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    return a.exec();
+    QString vmtFile = "/home/emredog/VMT_vid0179_1_enter-leave_78-87Union_181-464-548.pcd";
+
+    int widthOfVmt  = 181;
+    int heightOfVmt = 464;
+    int depthOfVmt  = 548;
+
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZI>);
+
+    if (pcl::io::loadPCDFile<pcl::PointXYZI> (vmtFile.toStdString(), *cloud) == -1) //* load the file
+    {
+        PCL_ERROR ("Couldn't read file\n");
+        return -1;
+    }
+
+    Vmt myVmt(cloud, widthOfVmt, heightOfVmt, depthOfVmt);
+
+    OrientationNormalizer orNor;
+
+    QList<float> angles;
+    angles << 0.0; //around X
+    angles << 2.0; //around Y
+    angles << 0.0; //around Z
+    Vmt rotatedVmt = orNor.rotateVmt(angles, myVmt);
+
+    pcl::io::savePCDFile("/home/emredog/VMT_vid0179_1_enter-leave_78-87Union_181-464-548-ROTATED-Y.pcd",
+                         *(rotatedVmt.getPointCloud_Const()));
+
 }
--- NormalizeOrientation_VMT/orientationnormalizer.h
+++ NormalizeOrientation_VMT/orientationnormalizer.h
@@ -11,6 +11,8 @@ public:
     OrientationNormalizer();
 
     QList<float> calculateRotationAngles(Vmt vmt);
+
+    //there should be 3 angle values for around X, Y and Z axises. "1.0" means 1 radian (~57.3 degrees)
     Vmt rotateVmt(QList<float> angles, const Vmt& initialVmt);
 
     //there should be 3 offset values for X, Y and Z. "1.0" means 1 meter.
